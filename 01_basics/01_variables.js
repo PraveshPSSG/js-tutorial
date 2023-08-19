93688c1a719f5015df26fead4273286ccb0d8367
PRAVESH <110671839+PraveshPSSG@users.noreply.github.com>
2023-08-19 06:38:37 +0000

    variable understanding

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12444
+let accountEmail="pravesh@gmail.com"
+var accountPassword="@14588"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 //not allowed
+/* prefer not to use var keyword because of block or functional scope */
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
